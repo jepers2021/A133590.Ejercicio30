1e684c973df0a12027358819f733cc07da51f2a3
Jorge <jepers2021@gmail.com>
2021-03-29 16:36:04 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A133590.Ejercicio30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 30");
+            Console.WriteLine("Programa que presenta la sólo la hora, dada una fecha.");
+
+            Console.Write("Por favor, ingrese una fecha: ");
+            DateTime fecha;
+            if (!DateTime.TryParse(Console.ReadLine(), out fecha))
+            {
+                Console.WriteLine("La fecha ingresada no es válida.");
+                Console.WriteLine("Presione cualquier tecla para continuar..");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Hora: {fecha.ToString("hh:mm tt")}");
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
